e8f1c3ab151418b685ae3ce8d3d25c8bd95b9384
edwardm8 <edwardm825@gmail.com>
2023-10-26 09:52:07 -0600

    added gallery

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 ## Attributions
 - [Nav bar](https://dev.to/devggaurav/let-s-build-a-responsive-navbar-and-hamburger-menu-using-html-css-and-javascript-4gci)
 - [Hamburger animation](https://www.w3schools.com/howto/howto_css_menu_icon.asp)
+- [Prepend](https://stackoverflow.com/questions/2007357/how-to-set-dom-element-as-first-child)
--- script.js
+++ script.js
@@ -1,14 +1,66 @@
+"use strict"
+
 const hamburger = document.querySelector(".hamburger")
 const navMenu = document.querySelector(".nav-menu")
 
-hamburger.addEventListener("click", mobileMenu);
+const imgArray = new Array()
+
+for(let i = 0;i <= 8;i++) {
+  imgArray[i] = new Image()
+  if(i==0) {
+    imgArray[i].src = 'assets/images/chocolate.png'
+    document.getElementById("chocolate").prepend(imgArray[i])
+  }
+  else if(i==1) {
+    imgArray[i].src = 'assets/images/caramel donut.png'
+    document.getElementById("caramel").prepend(imgArray[i])
+  }
+  else if(i==3) {
+    imgArray[i].src = 'assets/images/berry.png'
+    document.getElementById("berry").prepend(imgArray[i])
+  }
+  else if(i==4) {
+    imgArray[i].src = 'assets/images/coconut.png'
+    document.getElementById("coconut").prepend(imgArray[i])
+  }
+  else if(i==5) {
+    imgArray[i].src = 'assets/images/glaze.png'
+    document.getElementById("glaze").prepend(imgArray[i])
+  }
+  else if(i==6) {
+    imgArray[i].src = 'assets/images/lemon.png'
+    document.getElementById("lemon").prepend(imgArray[i])
+  }
+  else if(i==7) {
+    imgArray[i].src = 'assets/images/sprinkle.png'
+    document.getElementById("sprinkle").prepend(imgArray[i])
+  }
+  else if(i==8) {
+    imgArray[i].src = 'assets/images/powder.png'
+    document.getElementById("powder").prepend(imgArray[i])
+  }
+  else{
+    imgArray[i].src = 'assets/images/Maple-Cream-Donut-min.png'
+    document.getElementById("maple").prepend(imgArray[i])
+  }
+}
+
+hamburger.addEventListener("click", mobileMenu)
 
 function mobileMenu() {
-  navMenu.classList.toggle("active");
+  navMenu.classList.toggle("active")
 }
 
 function hamToggle(x){
   x.classList.toggle("change")
 }
 
+const ctaButton = document.querySelector('.cta')
 
+ctaButton.addEventListener('mouseover', ()=>{
+  ctaButton.style.backgroundColor = 'blue';
+});
+
+ctaButton.addEventListener('mouseout', () =>{
+  ctaButton.style.backgroundColor = 'red';
+});
